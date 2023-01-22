436abafa4dbfa70e980776f0f59c6c66e8b707e1
JODONG2 <whgusehd96@gmail.com>
2023-01-23 01:03:03 +0900

    Feat : User temp

--- BackEnd/src/main/java/com/a406/mrm/model/entity/User.java
+++ BackEnd/src/main/java/com/a406/mrm/model/entity/User.java
@@ -1,4 +1,38 @@
 package com.a406.mrm.model.entity;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name="users")
 public class User {
+    @Id
+    private String id;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String nickname;
+    @Column(nullable = false)
+    private String profile;
+    @Column(nullable = false)
+    private String intro;
+    @Column(nullable = false)
+    private String memo;
+    @ApiModelProperty("User - Room relation table mapping")
+    @OneToMany(mappedBy = "user")
+    private List<UserHasRoom> rooms = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Todo> todos = new ArrayList<>();
 }
