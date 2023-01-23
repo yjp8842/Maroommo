2c729680db7cf05be835a805cca49150ba13a423
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2023-01-24 00:18:25 +0900

    fix : ManyToOne fix

--- BackEnd/src/main/java/com/a406/mrm/model/entity/Category.java
+++ BackEnd/src/main/java/com/a406/mrm/model/entity/Category.java
@@ -20,8 +20,6 @@ public class Category {
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    // cascade 설정 안하면 FK로 쓰는 객체가 아직 저장이 안 되서 오류가 나서 설정
-    // 그런데 해당 설정하면 insert는 정상으로 작동하는데 room도 추가로 만들어버림 -> 수정 필요
     @JoinColumn(name = "group_id")
     private Room room;
 
