bbf0f7ac6b39404a9cf517748538fcbdfedacc95
smlee212 <smmylee212@naver.com>
2023-01-24 20:04:34 +0900

    Add : Login feature

--- BackEnd/src/main/java/com/a406/mrm/controller/UserController.java
+++ BackEnd/src/main/java/com/a406/mrm/controller/UserController.java
@@ -1,4 +1,95 @@
 package com.a406.mrm.controller;
 
+import com.a406.mrm.config.auth.PrincipalDetails;
+import com.a406.mrm.model.entity.User;
+import com.a406.mrm.repository.UserRepository;
+import com.a406.mrm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.Iterator;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping({ "", "/" })
+    public @ResponseBody String index() {
+        return "인덱스 페이지입니다.";
+    }
+
+    @GetMapping("/userinfo")
+    public @ResponseBody String userinfo(){
+        System.out.println();
+        return "유저 정보 페이지입니다";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principal) {
+        System.out.println("Principal : " + principal);
+        System.out.println("OAuth2 : "+principal.getUser().getProvider());
+//        // iterator 순차 출력 해보기
+//        Iterator<? extends GrantedAuthority> iter = principal.getAuthorities().iterator();
+//        while (iter.hasNext()) {
+//            GrantedAuthority auth = iter.next();
+//            System.out.println(auth.getAuthority());
+//        }
+
+        return "유저 페이지입니다.";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model,
+                        @RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "exception", required = false) String exception) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+        System.out.println("");
+        return "로그인 실패";
+    }
+
+    @PostMapping("/join")
+    public @ResponseBody String join(User user) {
+
+        String isJoin = userService.join(user);
+        System.out.println("회원가입 :"+isJoin);
+
+        return "/loginForm";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    // getName을 통해 로그인한 아이디를 불러올 수 있다
+    @GetMapping("/board")
+    public @ResponseBody String board(Principal principal, Authentication authentication){
+//          User activeUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//          System.out.println("로그인 유저 : "+activeUser);
+
+        System.out.println("principal : "+principal.getName());
+        System.out.println("authentication : "+authentication.getName());
+
+        return "로그인 성공";
+    }
+
 }
--- BackEnd/src/main/java/com/a406/mrm/repository/UserRepository.java
+++ BackEnd/src/main/java/com/a406/mrm/repository/UserRepository.java
@@ -2,8 +2,22 @@ package com.a406.mrm.repository;
 
 import com.a406.mrm.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    static final String UPDATE_USER_LAST_LOGIN = "update users set last_login_time = :lastLoginTime where id = :id";
+
+    @Transactional
+    @Modifying
+    @Query(value = UPDATE_USER_LAST_LOGIN, nativeQuery = true)
+    public int updateUserLastLogin(@Param("id") String id, @Param("lastLoginTime")LocalDateTime lastLoginTime);
+
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
