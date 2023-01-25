8e76279a70d5e4dc7d42601ef79833ee6509361a
JODONG2 <whgusehd96@gmail.com>
2023-01-25 23:31:24 +0900

    Feat : Change RoomDto to RoomRequestDto

--- BackEnd/src/main/java/com/a406/mrm/service/RoomService.java
+++ BackEnd/src/main/java/com/a406/mrm/service/RoomService.java
@@ -1,9 +1,10 @@
 package com.a406.mrm.service;
 
-import com.a406.mrm.model.dto.RoomDto;
+import com.a406.mrm.model.dto.RoomRequestDto;
+import com.a406.mrm.model.dto.RoomResponseDto;
 import com.a406.mrm.model.entity.Room;
 
 public interface RoomService {
-    Room makeRoom(RoomDto roomDto, String userId);
+    Room makeRoom(RoomRequestDto roomRequestDto, String userId);
     String modifyMemo(int roomId, String memo);
 }
