9446dfbcf0b5fca5fbd689fb38c0eefd177275ed
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2023-01-26 12:55:37 +0900

    fix : category entity dto fix

--- BackEnd/src/main/java/com/a406/mrm/controller/CategoryController.java
+++ BackEnd/src/main/java/com/a406/mrm/controller/CategoryController.java
@@ -1,61 +1,59 @@
 package com.a406.mrm.controller;
 
-import com.a406.mrm.model.dto.CategoryDto;
+import com.a406.mrm.model.dto.CategoryInsertDto;
 import com.a406.mrm.model.entity.Category;
-import com.a406.mrm.model.entity.Room;
-import com.a406.mrm.service.CategoryService;
+import com.a406.mrm.service.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("category/")
 public class CategoryController {
 
-    private final CategoryService categoryService;
+    private final CategoryServiceImpl categoryServiceImpl;
 
     @Autowired
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
+    public CategoryController(CategoryServiceImpl categoryServiceImpl) {
+        this.categoryServiceImpl = categoryServiceImpl;
     }
 
-    @GetMapping(value = "new")
-    public ModelAndView createForm() {
-        ModelAndView mav = new ModelAndView("category/createCategoryForm");
-        return mav;
-    }
+//    @GetMapping(value = "new")
+//    public ModelAndView createForm() {
+//        ModelAndView mav = new ModelAndView("category/createCategoryForm");
+//        return mav;
+//    }
 
     @PostMapping(value = "new")
-    public ResponseEntity<?> create(@RequestParam("name") String name, @RequestParam int roomid) {
-        CategoryDto categoryDto = new CategoryDto();
-        categoryDto.setName(name);
-        categoryDto.setRoomId(roomid);
-        return ResponseEntity.ok(categoryService.join(categoryDto,roomid));
+    public ResponseEntity<?> create(@RequestParam("name") String name, @RequestParam("room_id") int room_id) {
+        CategoryInsertDto categoryInsertDto = new CategoryInsertDto();
+        categoryInsertDto.setName(name);
+        categoryInsertDto.setRoomId(room_id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryInsertDto);
+//        return ResponseEntity.ok(categoryServiceImpl.join(categoryInsertDto,room_id));
     }
 
-    @GetMapping(value = "delete")
-    public ModelAndView deleteForm() {
-        ModelAndView mav = new ModelAndView("category/deleteCategoryForm");
-        return mav;
-    }
+//    @GetMapping(value = "delete")
+//    public ModelAndView deleteForm() {
+//        ModelAndView mav = new ModelAndView("category/deleteCategoryForm");
+//        return mav;
+//    }
 
     @DeleteMapping("delete/{id}")
-    public void delete(@PathVariable("id") int cid) {
-        categoryService.delete(cid);
+    public ResponseEntity<?> delete(@PathVariable("id") int cid) {
+        categoryServiceImpl.delete(cid);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @PatchMapping("update")
-    public void update(@RequestParam("id") int cid, @RequestParam("name") String name) {
-        categoryService.update(cid, name);
+    public ResponseEntity<?> update(@RequestParam("id") int cid, @RequestParam("name") String name) {
+        Category category = categoryServiceImpl.update(cid, name);
+        return ResponseEntity.status(HttpStatus.OK).body(category);
     }
 
     @GetMapping(value = "list")
-    public List<Category> Categorylist(@RequestBody Room room) {
-        List<Category> list = categoryService.findCategory(room.getId());
-        System.out.println(list);
-        return list;
+    public ResponseEntity<?> CategoryList(@RequestParam("room_id") int room_id) {
+        return ResponseEntity.ok(categoryServiceImpl.listCategory(room_id));
     }
 }
---
+++ BackEnd/src/main/java/com/a406/mrm/service/CategoryServiceImpl.java
@@ -0,0 +1,45 @@
+package com.a406.mrm.service;
+
+import com.a406.mrm.model.dto.CategoryInsertDto;
+import com.a406.mrm.model.entity.Category;
+import com.a406.mrm.repository.CategoryRepository;
+import com.a406.mrm.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
+    private final RoomRepository roomRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository, RoomRepository roomRepository) {
+        this.categoryRepository = categoryRepository;
+        this.roomRepository = roomRepository;
+    }
+
+    public CategoryInsertDto join(CategoryInsertDto categoryInsertDto, int roomId){
+        Category category = new Category(categoryInsertDto, roomRepository.findById(roomId));
+        return new CategoryInsertDto(categoryRepository.save(category));
+    }
+    public void delete(int id){
+        categoryRepository.deleteById(id);
+    }
+
+    public Category update(int id, String name){
+        Category category = categoryRepository.findById(id);
+        category.setName(name);
+        return category;
+    }
+
+    public List listCategory(int room_id) { return categoryRepository.findByroom_Id(room_id);}
+
+    public Category findOne(int id) {
+        return categoryRepository.findById(id);
+    }
+}
