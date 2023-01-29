141aaa5d9107d3c4af7dd0f931647053987a17f3
smlee212 <smmylee212@naver.com>
2023-01-29 14:19:08 +0900

    Feat : add function (find by id and pw)

--- BackEnd/src/main/java/com/a406/mrm/controller/UserController.java
+++ BackEnd/src/main/java/com/a406/mrm/controller/UserController.java
@@ -5,7 +5,6 @@ import com.a406.mrm.model.entity.User;
 import com.a406.mrm.service.EmailService;
 import com.a406.mrm.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -29,8 +28,39 @@ public class UserController {
 
 
     /**
+     *  아이디 중복 확인 메서드
+     *  동일한 아이디를 사용하는 유저가 존재하는지 확인한다
+     *  해당 유저가 존재하지 않는다면 (message:SUCCESS) 반환
+     *  존재한다면 (message:FAIL) 반환
+     */
+    @PostMapping("duplicate/id")
+    private ResponseEntity<Map<String, Object>> existsId(
+            @RequestBody User user) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            // 동일한 유저가 존재하지 않는다면
+            if(!userService.existsByUserForId(user.getId()))	{
+                resultMap.put("message", SUCCESS);
+                status = HttpStatus.ACCEPTED;
+            }
+            // 동일한 유저가 존재한다면
+            else {
+                resultMap.put("message", FAIL);
+                status = HttpStatus.ACCEPTED;
+            }
+        } catch (Exception e) {
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+
+    /**
      *  회원가입 메서드
-     *  아이디 중복 확인 후 비밀번호 암호화하여 DB에 저장
+     *  입력한 유저 정보를 바탕으로 회원가입을 진행한다
      */
     @PostMapping("join")
     private ResponseEntity<Map<String, Object>> join(
@@ -39,18 +69,10 @@ public class UserController {
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = null;
         try {
-            if(!userService.checkExistsUser(user.getId()))	{
-                userService.join(user);
+            userService.join(user);
 
-                resultMap.put("message", SUCCESS);
-                status = HttpStatus.ACCEPTED;
-            }
-            else {
-                resultMap.put("message", FAIL);
-                status = HttpStatus.ACCEPTED;
-            }
-
-
+            resultMap.put("message", SUCCESS);
+            status = HttpStatus.ACCEPTED;
         } catch (Exception e) {
             resultMap.put("message", e.getMessage());
             status = HttpStatus.INTERNAL_SERVER_ERROR;
@@ -58,8 +80,12 @@ public class UserController {
         return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
 
-    @PostMapping("/emailConfirm")
-    public @ResponseBody String emailConfirm(@RequestParam String email) throws Exception {
+    /**
+     *  이메일 전송 메서드
+     *  비밀번호 찾기 시 입력한 이메일 주소에 인증 코드를 전송
+     */
+    @PostMapping("emailConfirm")
+    private @ResponseBody String emailConfirm(@RequestParam String email) throws Exception {
 
         String confirm = emailService.sendMessage(email);
         System.out.println("이메일 전송 완료 : "+confirm);
@@ -67,8 +93,102 @@ public class UserController {
         return confirm;
     }
 
+    /**
+     *  아이디 찾기 메서드
+     *  아이디 찾기 시 입력한 이름, 이메일을 바탕으로 유저를 찾는다
+     *  해당되는 유저가 있을 경우 아이디와 (message:SUCCESS) 반환
+     *  없을 경우 (message:FAIL) 반환
+     */
+    @PostMapping("help/id")
+    private ResponseEntity<Map<String, Object>> findId(
+            @RequestBody String name, String email) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            User user = userService.findByUserForNameAndEmail(name,email).get();
+
+            // 해당 유저가 존재한다면
+            if(user != null){
+                String userId = user.getId();
+
+                int pos = 2;
+                String str = "***";
+
+                userId = userId.substring(0,pos) + str + str.substring(pos+str.length());
+
+                resultMap.put("id", userId);
+                resultMap.put("message", SUCCESS);
+                status = HttpStatus.ACCEPTED;
+            }
+            else {
+                resultMap.put("message", FAIL);
+                status = HttpStatus.ACCEPTED;
+            }
+        } catch (Exception e) {
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+    /**
+     *  비밀번호 찾기 메서드
+     *  비밀번호 찾기 시 입력한 아이디, 이름, 이메일에 해당되는 유저가 있는지 확인
+     *  해당되는 유저가 있을 경우 아이디와 (message:SUCCESS) 반환
+     *  없을 경우 (message:FAIL) 반환
+     */
+    @PostMapping("help/pw")
+    private ResponseEntity<Map<String, Object>> findPassword(
+            @RequestBody String id, String name, String email) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            // 해당 유저가 존재한다면
+            if(userService.existsByUserForIdAndNameAndEmail(id,name,email)){
+                resultMap.put("id",id);
+                resultMap.put("message", SUCCESS);
+                status = HttpStatus.ACCEPTED;
+            }
+            else {
+                resultMap.put("message", FAIL);
+                status = HttpStatus.ACCEPTED;
+            }
+        } catch (Exception e) {
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+    /**
+     *  비밀번호 변경 메서드
+     *  유저의 비밀번호를 변경한다
+     *  변경 성공 시 (message:SUCCESS) 반환
+     */
+    @PutMapping("help/pw")
+    private ResponseEntity<Map<String, Object>> modifyPassword(
+            @RequestBody String id, String password) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            userService.modifyPassword(id,password);
+
+            resultMap.put("message", SUCCESS);
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+
 
     // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    // 테스트 용 mapping 함수들
 
     @GetMapping({ "", "/" })
     public @ResponseBody String index() {
--- BackEnd/src/main/java/com/a406/mrm/service/UserService.java
+++ BackEnd/src/main/java/com/a406/mrm/service/UserService.java
@@ -2,8 +2,13 @@ package com.a406.mrm.service;
 
 import com.a406.mrm.model.entity.User;
 
+import java.util.Optional;
+
 public interface UserService {
 
-    public void join(User user) throws Exception;
-    public boolean checkExistsUser(String userid) throws Exception;
+    void join(User user) throws Exception;
+    boolean existsByUserForId(String userid) throws Exception;
+    Optional<User> findByUserForNameAndEmail(String name, String email) throws Exception;
+    boolean existsByUserForIdAndNameAndEmail(String id, String name, String email) throws Exception;
+    void modifyPassword(String id, String password) throws Exception;
 }
