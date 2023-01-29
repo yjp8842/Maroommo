d9eb8028b2169066265c427504c0cc07b9ed1f31
smlee212 <smmylee212@naver.com>
2023-01-29 14:19:08 +0900

    Feat : add function (find by id and pw)

--- BackEnd/src/main/java/com/a406/mrm/repository/UserRepository.java
+++ BackEnd/src/main/java/com/a406/mrm/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByProviderAndProviderId(String provider, String providerId);
+    Optional<User> findByNameAndEmail(String name, String email);
+    boolean existsByIdAndNameAndEmail(String id, String name, String email);
 }
--- BackEnd/src/main/java/com/a406/mrm/service/UserServiceImpl.java
+++ BackEnd/src/main/java/com/a406/mrm/service/UserServiceImpl.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
@@ -23,9 +25,30 @@ public class UserServiceImpl implements UserService{
 
     // 유저 id를 조회하여 동일한 유저가 있는지 확인한다
     @Override
-    public boolean checkExistsUser(String userid) throws Exception {
+    public boolean existsByUserForId(String userid) throws Exception {
         return userRepository.existsById(userid);
     }
+
+    // 이름과 이메일로 유저를 조회하여 유저를 반환
+    @Override
+    public Optional<User> findByUserForNameAndEmail(String name, String email) throws Exception {
+        return userRepository.findByNameAndEmail(name,email);
+    }
+
+    // 아이디와 이름, 이메일을 조회하여 유저가 존재하는지 확인
+    @Override
+    public boolean existsByUserForIdAndNameAndEmail(String id, String name, String email) throws Exception {
+        return userRepository.existsByIdAndNameAndEmail(id,name,email);
+    }
+
+    // 비밀번호 변경
+    @Override
+    public void modifyPassword(String id, String password) throws Exception {
+        User user = userRepository.findById(id).get();
+        user.setPassword(bCryptPasswordEncoder.encode(password));// 비밀번호 암호화
+        userRepository.save(user);
+    }
+
 
 }
 
