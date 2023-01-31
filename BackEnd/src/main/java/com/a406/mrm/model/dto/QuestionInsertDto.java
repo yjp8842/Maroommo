51e928f4f47f0bc064210a30efbdfe813f54bd00
Oh JUN Ho <dhwnsghdhkd@naver.com>
2023-02-01 01:22:14 +0900

    Feat : question crud function

---
+++ BackEnd/src/main/java/com/a406/mrm/model/dto/QuestionInsertDto.java
@@ -0,0 +1,37 @@
+package com.a406.mrm.model.dto;
+
+import com.a406.mrm.model.entity.Board;
+import com.a406.mrm.model.entity.Question;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionInsertDto {
+
+    public QuestionInsertDto(Question question) {
+        this.id = question.getId();
+        this.title = question.getTitle();
+        this.content = question.getContent();
+        this.createtime = question.getCreateTime();
+        this.status = question.getStatus();
+        this.picture = question.getPicture();
+        this.categorysub_id = question.getCategorySub().getId();
+        this.user_id = question.getUser().getId();
+    }
+
+    private int id;
+    private String title;
+    private String content;
+    private Date createtime;
+    private int status;
+    private String picture;
+    private int categorysub_id;
+    private String user_id;
+
+
+}
