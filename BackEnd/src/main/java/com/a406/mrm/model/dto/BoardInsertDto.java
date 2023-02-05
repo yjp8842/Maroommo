d4b39e3540e2ea429a0d0562c14f1b8620595717
Oh JUN Ho <dhwnsghdhkd@naver.com>
2023-02-06 01:07:02 +0900

    Refactor : board swagger update and dto update

--- BackEnd/src/main/java/com/a406/mrm/model/dto/BoardInsertDto.java
+++ BackEnd/src/main/java/com/a406/mrm/model/dto/BoardInsertDto.java
@@ -13,7 +13,6 @@ import java.util.Date;
 public class BoardInsertDto {
 
     public BoardInsertDto(Board board) {
-        this.id = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
         this.createtime = board.getCreateTime();
@@ -23,7 +22,6 @@ public class BoardInsertDto {
         this.user_id = board.getUser().getId();
     }
 
-    private int id;
     private String title;
     private String content;
     private Date createtime;
