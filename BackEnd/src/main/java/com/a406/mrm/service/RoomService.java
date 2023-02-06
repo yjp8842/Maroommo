1b34d87ba6f04673d0609d870bc80b42f2c98ecc
whgusehd96 <whgusehd96@gmail.com>
2023-02-06 15:59:39 +0900

    Feat : remove room memo column, modify profile

--- BackEnd/src/main/java/com/a406/mrm/service/RoomService.java
+++ BackEnd/src/main/java/com/a406/mrm/service/RoomService.java
@@ -2,12 +2,13 @@ package com.a406.mrm.service;
 
 import com.a406.mrm.model.dto.RoomRequestDto;
 import com.a406.mrm.model.entity.Room;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface RoomService {
     void removeRoom(int roomId);
     String modifyName(int roomId, String name);
     String modifyIntro(int roomId, String intro);
-    String modifyProfile(int roomId, String profile);
+    String modifyProfile(int roomId, MultipartFile profile);
     Room makeRoom(RoomRequestDto roomRequestDto, String userId);
     Room enterRoom(int roomId, String userId);
     boolean existsRoomByIdAndCode(int roomId, String code);
