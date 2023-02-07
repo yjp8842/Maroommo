4aff065edffd51b98c33614c126e8096b1d006c5
smmylee212@naver.com <smmylee212>
2023-02-07 16:23:36 +0900

    Feat : rename file name & add send Memo

--- BackEnd/src/main/java/com/a406/mrm/controller/StompController.java
+++ BackEnd/src/main/java/com/a406/mrm/controller/StompController.java
@@ -1,8 +1,10 @@
 package com.a406.mrm.controller;
 
 import com.a406.mrm.model.dto.ChatMessageRequestDto;
+import com.a406.mrm.model.dto.MemoDto;
 import com.a406.mrm.service.ChatMessageService;
 
+import com.a406.mrm.service.MemoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -23,12 +25,13 @@ import java.util.Map;
 @RestController
 @Api("Chat Message Controller API")
 @RequiredArgsConstructor
-public class ChatMessageController {
+public class StompController {
 
     private static final Logger logger = LoggerFactory.getLogger(RoomController.class);
 
     private final SimpMessageSendingOperations sendingOperations;
     private final ChatMessageService chatMessageService;
+    private final MemoService memoService;
 
     /**
      *  채팅 보내기 메서드
@@ -56,6 +59,38 @@ public class ChatMessageController {
 
         return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
+    /**
+     *  메모 보내기 메서드
+     *  /app/chat/memo로 요청이 오면
+     *  해당 memo를 db에 저장하고, memo를 전송한다
+     *
+     *  회원 메모라면 userId가 존재하고
+     *  그룹 메모라면 userId = ""이라고 가정한다
+     */
+    @ApiOperation("Send Memo")
+    @MessageMapping("/chat/memo")
+    public ResponseEntity<Map<String, Object>> sendMemo(
+            @ApiParam("Send Memo") MemoDto memo) {
+        logger.info("[sendMemo] 메모 발송 : "+memo);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            // DB에 메모 저장
+            memoService.insertMemo(memo);
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        // 메시지를 보낸다
+        sendingOperations.convertAndSend("/sub/memo/room/"+memo.getRoomId(),memo);
+
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+
 
     // swagger test용 send
     @ApiOperation("Tes Send Chatting Message")
@@ -70,6 +105,27 @@ public class ChatMessageController {
         try {
             // DB에 메시지 저장
             chatMessageService.insertChat(message);
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+    @ApiOperation("Tes Send Memo")
+    @PostMapping("/chat/memo")
+    public ResponseEntity<Map<String, Object>> sendTestMemo(
+            @RequestBody @ApiParam("Send Message") MemoDto memo) {
+        logger.info("[sendTestMemo] 메시지 발송 : "+memo);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+
+        try {
+            // DB에 메시지 저장
+            memoService.insertMemo(memo);
             status = HttpStatus.ACCEPTED;
         } catch (Exception e) {
             resultMap.put("error", e.getMessage());
