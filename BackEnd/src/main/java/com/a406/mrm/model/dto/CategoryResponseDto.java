cf0e92dd299c1eba1907d510cefe828537ea0c1b
Oh JUN Ho <dhwnsghdhkd@naver.com>
2023-02-09 00:19:14 +0900

    Feat : response category dto update

--- BackEnd/src/main/java/com/a406/mrm/model/dto/CategoryResponseDto.java
+++ BackEnd/src/main/java/com/a406/mrm/model/dto/CategoryResponseDto.java
@@ -1,16 +1,13 @@
 package com.a406.mrm.model.dto;
 
 import com.a406.mrm.model.entity.Category;
-import com.a406.mrm.model.entity.CategorySub;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Data
@@ -21,19 +18,12 @@ public class CategoryResponseDto {
     public CategoryResponseDto(Category category){
         this.id = category.getId();
         this.name = category.getName();
-//        this.roomId = category.getRoom().getId();
-//        this.categorySubs_id = category.getCategorySubs().stream().map(x -> x.getId()).collect(Collectors.toList());
-//        this.categorySub_name = category.getCategorySubs().stream().map(x -> x.getName()).collect(Collectors.toList());
-        this.categorySubs = Map.of(category.getCategorySubs().stream().map(x -> x.getId()).collect(Collectors.toList()),category.getCategorySubs().stream().map(x -> x.getName()).collect(Collectors.toList()));
+        this.categorySubs = category.getCategorySubs().stream().map(x->new CategorySubResponseDto(x)).collect(Collectors.toList());
     }
 
     private int id;
     private String name;
-//    private int roomId;
-
-//    private List<Integer> categorySubs_id = new ArrayList<>();
-//    private List<String> categorySub_name = new ArrayList<>();
-    private Map<List<Integer>, List<String>> categorySubs = new HashMap<>();
+    private List<CategorySubResponseDto> categorySubs = new ArrayList<>();
 
 
 }
