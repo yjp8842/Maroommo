ac4a8e30a41d8920ad008a313c750d0c16f29533
JODONG2 <whgusehd96@gmail.com>
2023-02-10 21:33:43 +0900

    Feat : add config commons multipart resolver

--- BackEnd/src/main/java/com/a406/mrm/config/WebMvcConfig.java
+++ BackEnd/src/main/java/com/a406/mrm/config/WebMvcConfig.java
@@ -3,7 +3,9 @@ package com.a406.mrm.config;
 
 import io.swagger.models.HttpMethod;
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,4 +23,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .maxAge(3000);
     }
 
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setDefaultEncoding("UTF-8");
+        multipartResolver.setMaxUploadSizePerFile(5*1024*1024);
+        return multipartResolver;
+    }
 }
