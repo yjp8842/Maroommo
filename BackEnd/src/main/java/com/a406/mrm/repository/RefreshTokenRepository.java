2f71a56c1c9c95a31fb638e4f13c9c9d378e7322
smlee212 <smmylee212@naver.com>
2023-02-11 22:08:18 +0900

    Add : jwt token

---
+++ BackEnd/src/main/java/com/a406/mrm/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.a406.mrm.repository;
+
+import com.a406.mrm.model.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
