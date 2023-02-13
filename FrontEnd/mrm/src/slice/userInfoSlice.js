7a1f6ab4d1b9fc6f32927b18845f002543ceb287
sinecure96 <sinecure96@gmail.com>
2023-02-13 11:01:12 +0900

    Feat: user data storage

---
+++ FrontEnd/mrm/src/slice/userInfoSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userInfoSlice = createSlice({
+
+  name: 'user',
+  initialState: {
+
+  },
+  reducers: {
+
+  }
+})
+
+
+export const userInfoReducers = userInfoSlice.reducer;
+export const userInfoActions = userInfoSlice.actions;
