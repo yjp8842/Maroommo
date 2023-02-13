5334a77646ba01060272a484516885910a7af0e4
sinecure96 <sinecure96@gmail.com>
2023-02-13 11:00:19 +0900

    Feat: qna files

--- FrontEnd/mrm/src/slice/rootSlice.js
+++ FrontEnd/mrm/src/slice/rootSlice.js
@@ -6,8 +6,9 @@ import { questionReducers } from "./questionSlice";
 import { questionArticleReducers } from "./questionArticleSlice";
 // import { questionCommentReducers } from "./questionCommentSlice";
 import { answerReducers } from "./answerSlice";
+import { userInfoReducers } from "./userInfoSlice";
 
-const rootReducer = combineReducers({articleReducers, boardReducers, commentReducers, questionReducers, questionArticleReducers, answerReducers})
+const rootReducer = combineReducers({articleReducers, boardReducers, commentReducers, questionReducers, questionArticleReducers, answerReducers, userInfoReducers})
 
 
 export default rootReducer;
