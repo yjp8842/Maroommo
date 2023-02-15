1fdb4ab8481d2a10e563017c772f072d4367b687
whgusehd96 <whgusehd96@gmail.com>
2023-02-15 17:43:16 +0900

    Feat : modify total minute

--- BackEnd/src/main/java/com/a406/mrm/model/dto/TodoTimeDto.java
+++ BackEnd/src/main/java/com/a406/mrm/model/dto/TodoTimeDto.java
@@ -4,6 +4,8 @@ import com.a406.mrm.model.entity.TodoTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -12,6 +14,7 @@ import java.time.ZoneId;
 @Setter
 @NoArgsConstructor
 public class TodoTimeDto {
+    private Logger logger = LoggerFactory.getLogger(TodoTimeDto.class);
     private String userId ;
     private int todoId;
     private LocalDateTime startTime;
@@ -25,7 +28,7 @@ public class TodoTimeDto {
         this.totalMinute = 0;
         if(this.endTime!=null){
             LocalDateTime TIME = LocalDateTime.ofInstant(todoTime.getTotalTime().toInstant(), ZoneId.systemDefault());
-            this.totalMinute = TIME.getHour() * 60 + TIME.getMinute() ;
+            this.totalMinute = (TIME.getHour()-9) * 60 + TIME.getMinute() ;
         }
     }
 }
